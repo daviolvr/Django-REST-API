f806b4a958ab36e13b99911e432ace6685a29838
Davi de Oliveira <olidavi031130@gmail.com>
2024-09-01 21:35:15 -0300

    initial

---
+++ watchlist_app/api/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from watchlist_app.api.views import (WatchListAV, WatchDetailsAV, 
+                                     StreamPlatformAV, StreamPlatformDetailAV, 
+                                     ReviewDetail, ReviewList, ReviewCreate,
+                                     StreamPlatformVS)
+# from watchlist_app.api.views import movie_list, movie_details
+
+router = DefaultRouter()
+router.register('stream', StreamPlatformVS, basename='streamplatform')
+
+urlpatterns = [
+    path('list/', WatchListAV.as_view(), name='movie-list'),
+    path('<int:pk>', WatchDetailsAV.as_view(), name='movie-details'),
+    path('', include(router.urls)),
+    
+    # path('stream/', StreamPlatformAV.as_view(), name='stream'),
+    # path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream-details'),
+    # path('review/', ReviewList.as_view(), name='review-list'),
+    # path('review/<int:pk>', ReviewDetail.as_view(), name='review-details'),
+    
+    path('stream/<int:pk>/review-create/', ReviewCreate.as_view(), name='review-create'),
+    path('stream/<int:pk>/review/', ReviewList.as_view(), name='review-list'),
+    path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-details'),
+]
